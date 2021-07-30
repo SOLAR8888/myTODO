6955d68d0fd635f4aba75b8e963552ddc74d4654
SOLAR <solar88888@gmail.com>
2021-07-30 17:24:22 +0300

    simple app

---
+++ solar-todo-client/src/components/Navbar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Navbar = () => (
+    <nav className='navbar navbar-dark navbar-expand-lg bg-dark'>
+        <div className='navbar-brand ml-4'>
+            TODOwe4ka
+        </div>
+    </nav>
+)
