3b183f25cc76509d68482679f3d8cf5ff07da2a3
SOLAR <solar88888@gmail.com>
2021-08-06 16:12:30 +0300

    server auth

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -18,7 +18,7 @@ router.post('/register',
       if(!errors.isEmpty()){
           return res.status(400).json({
               errors:errors.array(),
-              message:'Некорректные данные регистрации'
+              message:errors.array()[0].msg
           })
       }
       const {email, password} = req.body;
